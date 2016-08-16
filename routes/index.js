6ffe73f930d97c8c34e6a900848aa7eff5784389
Dan Sajjad <dan.sajjad@gmail.com>
2016-08-16 12:42:57 -0400

    Update firebase connection for firebase v3.x

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require('firebase');
+var firebaseCred = require('./../firebaseSAC');
 
-var firebaseRef = new Firebase('https://myapp-b5edd.firebaseio.com/');
+// firebase.initializeApp({
+//   servericeAccount: firebaseCred,
+//   databaseUrl: 'https://myapp-b5edd.firebaseio.com/' 
+// });
 
-var firebaseContent = {};
-var firebaseRef.on('value', function (snapshot) {
-  firebaseContent = snapshot.val();
-}, function(error) {
-  console.log('The read failed: ', error.code);
-})
+// var db = firebase.database();
+// var ref = db.ref("restricted_access/secret_document");
+// ref.once("value", function(snapshot) {
+//   console.log(snapshot.val());
+// }, function(error) {
+//   console.log('The read failed: ', error.code);
+// });
+
+
+
+
+
+// Initialize the app with no authentication
+firebase.initializeApp({
+  databaseURL: "https://myapp-b5edd.firebaseio.com/"
+});
+
+// The app only has access to public data as defined in the Security Rules
+var db = firebase.database();
+var ref = db.ref("/some_public_resource");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
+
+
 
 
 /* GET home page. */
